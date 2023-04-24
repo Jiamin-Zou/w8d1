f396634b0f3adeb20cbdedeb66fe42ab369ee306
Matt Pierson <matt.pierson789@gmail.com>
2023-04-24 17:07:49 -0400

    start

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,25 +8,70 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+
+    @req = req
+    @res = res
+
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+
+    @already_built_response ||= false
+
+    
   end
 
   # Set the response status code and header
   def redirect_to(url)
+
+    raise "already rendered" if @already_built_response
+
+    @res.status = 302 
+    @res["Location"] = url
+    @already_built_response = true
+    nil
+
+
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+
+    raise "already rendered" if @already_built_response
+
+    @res.write(content)
+    @res["Content-Type"] = content_type
+    @already_built_response = true
+    nil
+
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+
+    raise "already rendered" if @already_built_response
+
+    controller_name = self.to_s
+
+    array = []
+
+    template_path = ['views', controller_name, "#[template_name}.html.erb"].join("/")    
+    
+    template = ERB.new(File.read(template_path))
+
+    #render the template the the current binding 
+    html = template.result(binding)
+
+    #pass the HTML to the render content method 
+
+    render_content(html, 'text/html')
+
+    @already_built_response = true
+
   end
 
   # method exposing a `Session` object
